e343597d23ef9b0801c20a9cba3407c31dd33ccf
martinpacheco-atos <angel-martin.pacheco@atos.net>
2022-07-22 11:19:07 -0500

    3. The list of countries must be ordered
    The list of countries must be ordered by its official name in ascending mode.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const countries = [];
+let countries = [];
 
 async function getAllCountries(){
 
@@ -7,40 +7,72 @@ async function getAllCountries(){
     const response = await fetch('https://restcountries.com/v3.1/all')
     const cts = await response.json()
 
-    
-    console.log(cts)
-   
- 
-    const table = document.getElementById('countries');
+
 
     for(const country of cts){
 
+        let ct = {} 
+        ct.name = country.name.official;
+        ct.capital = country.capital ? country.capital[0] : 'No capital'
+        ct.region = country.region
+        ct.language  = country.languages ? Object.values(country.languages).toString() : 'No language to display'
+        ct.population = country.population
+        ct.flag = country.flags.png
+        countries.push(ct)
+    }
+
+    countries = countries.sort(sortArrayAscending)
+
+
+    const table = document.getElementById('countries');
+
+
+    for(const country of countries){
+
+
         let row = table.insertRow();
         row.classList.add('even:bg-zinc-200')
+        
+
         let officialName = row.insertCell()
-        officialName.innerText = country.name.official;
+        officialName.innerText = country.name;
+        
 
         let capital = row.insertCell()
-        capital.innerText = country.capital ? country.capital[0] : 'No capital'
+        capital.innerText = country.capital
+        
 
         let region = row.insertCell()
         region.innerText = country.region
+       
 
         let language = row.insertCell()
-        language.innerText = country.languages ? Object.values(country.languages).toString() : 'No language to display'
+        language.innerText = country.language
         
+
         let population = row.insertCell()
         population.innerText = country.population
+        
 
         let flag = row.insertCell()
 
         let img = document.createElement("img");
-        img.src = country.flags.png
+        img.src = country.flag
+        img.style.minWidth = "100px";
         flag.appendChild(img) 
-
+       
     }
-    
+  
 }
+
+
+
+function sortArrayAscending(x, y){
+    if (x.name < y.name) {return -1;}
+    if (x.name > y.name) {return 1;}
+    return 0;
+}
+
 
 getAllCountries()
 
