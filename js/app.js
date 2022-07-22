495fa6f9e022a1729831376058a5397a5d355a7f
martinpacheco-atos <angel-martin.pacheco@atos.net>
2022-07-22 10:39:53 -0500

    2. Show all the countries on a table
    Show all the countries on a table, and the data to display is the following:
    a. Official Name
    b. Capital. If doesn’t Have a capital, show “No capital”
    c. Region
    d. Language. If it doesn’t have a language, show “No language to display”
    e. Population
    f. Flag

--- js/app.js
+++ js/app.js
@@ -1,15 +1,44 @@
 
-
+const countries = [];
 
 async function getAllCountries(){
 
 
     const response = await fetch('https://restcountries.com/v3.1/all')
-    const countries = await response.json()
+    const cts = await response.json()
 
     
-    console.log(countries)
+    console.log(cts)
    
+ 
+    const table = document.getElementById('countries');
+
+    for(const country of cts){
+
+        let row = table.insertRow();
+        row.classList.add('even:bg-zinc-200')
+        let officialName = row.insertCell()
+        officialName.innerText = country.name.official;
+
+        let capital = row.insertCell()
+        capital.innerText = country.capital ? country.capital[0] : 'No capital'
+
+        let region = row.insertCell()
+        region.innerText = country.region
+
+        let language = row.insertCell()
+        language.innerText = country.languages ? Object.values(country.languages).toString() : 'No language to display'
+        
+        let population = row.insertCell()
+        population.innerText = country.population
+
+        let flag = row.insertCell()
+
+        let img = document.createElement("img");
+        img.src = country.flags.png
+        flag.appendChild(img) 
+
+    }
     
 }
 
